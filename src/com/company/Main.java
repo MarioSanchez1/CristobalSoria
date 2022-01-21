7f8ef617b272774c2a223b8d7ae6a019aaf81c02
Oscar <osanz@padredamiansscc.net>
2022-01-21 10:34:05 +0100

    Oscar Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hola bon dias");
         System.out.println("Oscar Sanz Sanchez");
+        System.out.println("Jorge D'Alessandro");
     }
 }
