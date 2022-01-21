c7d0e16cb96ccfc45269d1e3342473cee38b5a3f
Oscar <osanz@padredamiansscc.net>
2022-01-21 10:35:00 +0100

    Oscar Commit

--- src/com/company/HelloDAM1.java
+++ src/com/company/HelloDAM1.java
@@ -5,6 +5,5 @@ public class HelloDAM1 {
     public static void main(String[] args) {
         System.out.println("Luis Sanchez");
         System.out.println("Oscar Sanz Sanchez");
-        System.out.println("Cristobal Soria");
     }
 }
