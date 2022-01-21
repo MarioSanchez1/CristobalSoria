a6b4c8e3a2eb0b63bdd2e10c8e1d802c48a5f543
sanch <sanchez_erazo@hotmail.com>
2022-01-21 10:30:34 +0100

    tercer commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola bon dias");
+        System.out.println("Luis Sanchez");
         System.out.println("Oscar Sanz Sanchez");
     }
 }
